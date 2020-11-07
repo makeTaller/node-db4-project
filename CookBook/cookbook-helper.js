a17ecb17c1f2f11790a1211cf5779b5ebb69679a
Kirk Tolliver <kirktolliver28@gmail.com>
2020-11-07 12:48:17 -0600

    Added a table to get quantity and  changed find to getRecipe

--- CookBook/cookbook-helper.js
+++ CookBook/cookbook-helper.js
@@ -1,16 +1,14 @@
-const Recipes = require("../kconfig")
+const Cookbook = require("../kconfig")
 
+module.exports = {
+  getRecipes 
+} 
 
-function find() {
-    Recipes("Recipe")
+function getRecipes() {
+    return Cookbook("Recipe")
 }    
 
 
 // - getRecipes()`: should return a list of all recipes in the database.
 // - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
 // - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
-
-
-module.exports = {
-   find
-} 
--- CookBook/cookbook-router.js
+++ CookBook/cookbook-router.js
@@ -5,11 +5,13 @@ const db = require("./cookbook-helper")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    db.find()
+    db.getRecipes()
     .then( (recipe) => {
-        res.json("message", recipe )
-    }
-    )
+        res.json(recipe )
+    })
+    .catch( ( error ) => {
+        res.status(500).json({ message: "failed to get db"})
+    } )
 })
 
 module.exports = router
